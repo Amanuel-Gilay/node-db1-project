69c059e9c55873555ea7caee8025520879de014b
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-10 13:21:04 -0700

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,18 @@
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log('checkAcountPayload middleware')
+  next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log('checkAcountPayload middleware')
+  next()
+
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log('checkAcountPayload middleware')
+  next()
 }
