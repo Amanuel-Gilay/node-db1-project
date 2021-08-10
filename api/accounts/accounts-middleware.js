67969c26aef90db2b92c5fae4daf922caa8b4ae9
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-10 14:37:59 -0700

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -5,22 +5,22 @@ exports.checkAccountPayload = (req, res, next) => {
   const {name, badget } = req.body
   if (name === undefined || badget === undefined) {
     error.message = 'name and budjet are required'
-    next(error)
   }else if (typeof name !== 'string') {
     error.message = 'name of account must be a string'
-    next(error)
-
   }else if (name.trim().length <3 || name.trim().length>100) {
     error.message = 'name of account must be between 3 and 100'
-    next(err)
   }else if (typeof budget !== 'number' || isNaN(badget)) {
     error.message = 'budget of account must be a number'
-    next(error)
   }else if (budget < 0 || budget > 1000000) {
     error.message = 'budget of account is too large or too small'
-    next(error)
     
   }
+}
+
+if (error) {
+  next(error)
+}else {
+  next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
