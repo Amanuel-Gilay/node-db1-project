eb724c99e47163013f6260fa3d1c85db9cc597b0
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-10 14:33:44 -0700

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,9 +1,26 @@
 const Account = require('./accounts-model')
 
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('checkAcountPayload middleware')
-  next()
+  const error = {status: 400 }
+  const {name, badget } = req.body
+  if (name === undefined || badget === undefined) {
+    error.message = 'name and budjet are required'
+    next(error)
+  }else if (typeof name !== 'string') {
+    error.message = 'name of account must be a string'
+    next(error)
+
+  }else if (name.trim().length <3 || name.trim().length>100) {
+    error.message = 'name of account must be between 3 and 100'
+    next(err)
+  }else if (typeof budget !== 'number' || isNaN(badget)) {
+    error.message = 'budget of account must be a number'
+    next(error)
+  }else if (budget < 0 || budget > 1000000) {
+    error.message = 'budget of account is too large or too small'
+    next(error)
+    
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
