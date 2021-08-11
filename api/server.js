5963490d45a22df63ad264f1c15f3c38a8993e72
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-10 18:57:43 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-server.use('/api/accounts',)
+server.use('/api/accounts', accountsRouter);
 
 server.use('*', (req, res) => {
     res.status(404).json({
